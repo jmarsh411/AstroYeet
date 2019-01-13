8ec39ef0e9d0e02a33bf806eab4a0c2130160312
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-01-13 16:36:45 -0500

    added lasers for visual demo

---
+++ Assets/Scripts/SpawnLasers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnLasers : MonoBehaviour {
+
+    [Header("Set in Inspector")]
+    public GameObject AsterPrefab;
+    public float spawnPerSec = 1f;
+    public float spawnPadding = 1.5f;
+    public Vector3 spawnZone;
+
+    void Awake()
+    {
+        Invoke("SpawnLaser", 1f / spawnPerSec);
+    }
+
+    void SpawnLaser()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnZone.x, spawnZone.x), spawnZone.y, spawnZone.z);
+        Instantiate(AsterPrefab, spawnPos, Quaternion.identity);
+        Invoke("SpawnLaser", 1f / spawnPerSec);
+    }
+}
