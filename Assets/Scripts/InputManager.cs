c08397fac29dcb251bd2bd64c488e397d1cffef6
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-02-08 23:12:38 -0500

    decoupled input from player movement, improved boost

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    bool isBoosting;
+    bool canBoost;
+    public float boostTimeout;
+    public GameObject playerShip;
+    PlayerMovement movement;
+    Vector3 move;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isBoosting = false;
+        canBoost = true;
+        movement = playerShip.GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // If boost button pressed and not already boosting and I'm allowed to boost
+        // then boost
+        movement.horizAxis = Input.GetAxis("Horizontal");
+
+        if (Input.GetAxis("BoostJoy") > 0 || Input.GetAxis("BoostKey") > 0)
+        {
+            if (!isBoosting && canBoost)
+            {
+                Boost();
+            }
+        }
+    }
+
+
+    void Boost()
+    {
+        Debug.Log("Boosting");
+        movement.Boost();
+        isBoosting = true;
+        canBoost = false;
+        Invoke("StopBoost", boostTimeout);
+    }
+
+    void StopBoost()
+    {
+        Debug.Log("Stopped Boosting");
+        isBoosting = false;
+        canBoost = true;
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
-
+    readonly float defaultSpeed = 5f;
     public float speed = 5.0f;
+    public float horizAxis;
     Vector3 move;
 
     // Use this for initialization
@@ -14,14 +15,18 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetAxis("BoostJoy") > 0 || Input.GetAxis("BoostKey") > 0)
-        {
-            speed = 30.0f;
-        }
-        else
-            speed = 5.0f;
+        move = new Vector3(horizAxis, 0, 0);
+        transform.position += move * speed * Time.deltaTime;
+    }
 
-        move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
-		transform.position += move * speed * Time.deltaTime;
+    public void Boost()
+    {
+        speed = defaultSpeed * 6;
+        Invoke("StopBoost", 0.08f);
+    }
+
+    public void StopBoost()
+    {
+        speed = defaultSpeed;
     }
 }
