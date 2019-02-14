28f815f0ec33c22e049547d9bdafca107e09fc98
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-02-13 22:04:12 -0500

    added test fade script for laser warnings

---
+++ Assets/TestFade.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFade : MonoBehaviour
+{
+    public float fadeTime;
+    public float targetAlpha;
+
+    private Color color;
+    private SpriteRenderer sprite;
+    private float alpha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(FadeTo(1f, 3f));  // hardcoded for now
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FadeTo(float aValue, float aTime)
+    {
+        alpha = sprite.color.a;
+        for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+        {
+            color = sprite.color;
+            sprite.color = new Color(color.r, color.g, color.b, Mathf.Lerp(alpha, aValue, t));
+            yield return null;
+        }
+    }
+}
