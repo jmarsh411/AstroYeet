927ce47552f2e106fc119905d52bafcd4cb6aa96
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-02-14 22:23:52 -0500

    further generalized FadeSprite to work with SpriteRenderer or UI Image

--- Assets/Scripts/FadeSprite.cs
+++ Assets/Scripts/FadeSprite.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeSprite : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class FadeSprite : MonoBehaviour
 
     private Color color;
     private SpriteRenderer sprite;
+    private Image image;
     private float alpha;
     private bool faded;
 
@@ -19,6 +21,18 @@ public class FadeSprite : MonoBehaviour
         // hardcoded for now
         SetFadeParams(3f, 1f);
         sprite = GetComponent<SpriteRenderer>();
+        // set to Spriterenderer's color if the component loaded
+        if (sprite != null)
+        {
+            color = sprite.color;
+
+        }
+        // if no SpriteRenderer, use Image's color instead (for UI images)
+        else
+        {
+            image = GetComponent<Image>();
+            color = image.color;
+        }
         StartCoroutine(FadeTo(targetAlpha, fadeTime));
     }
 
@@ -31,11 +45,18 @@ public class FadeSprite : MonoBehaviour
     // https://answers.unity.com/questions/654836/unity2d-sprite-fade-in-and-out.html?childToView=654842#
     IEnumerator FadeTo(float aValue, float aTime)
     {
-        alpha = sprite.color.a;
+        alpha = color.a;
         for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
         {
-            color = sprite.color;
-            sprite.color = new Color(color.r, color.g, color.b, Mathf.Lerp(alpha, aValue, t));
+            // adjust the alpha towards its target value
+            color = new Color(color.r, color.g, color.b, Mathf.Lerp(alpha, aValue, t));
+
+            // set the appropriate color, depending on what component exists
+            if (sprite != null)
+                sprite.color = color;
+            if (image != null)
+                image.color = color;
+
             yield return null;
         }
         faded = true;
