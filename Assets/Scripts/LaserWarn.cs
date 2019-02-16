5f04d2052543f6b33fa1396b0ee8e5a2bc72e53e
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-02-15 21:59:37 -0500

    laser warning blink is now configurable and works properly

--- Assets/Scripts/LaserWarn.cs
+++ Assets/Scripts/LaserWarn.cs
@@ -6,17 +6,20 @@ public class LaserWarn : MonoBehaviour
 {
     SpawnLasers lasers;
     Vector3 laserPos;
+    Animator anim;
     private float laserYOffset;
 
     private void Awake()
     {
         laserYOffset = -3f;
+        anim = GetComponent<Animator>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         lasers = Camera.main.GetComponent<SpawnLasers>();
+        StartCoroutine(BlinkAfter(2.15f));
         StartCoroutine(DestroyAfter(3f));
     }
 
@@ -24,6 +27,12 @@ public class LaserWarn : MonoBehaviour
     void Update()
     {
         
+    }
+
+    IEnumerator BlinkAfter(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        anim.SetBool("isBlinking", true);
     }
 
     IEnumerator DestroyAfter(float waitTime)
