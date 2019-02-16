67f3323adfadbdb207a549744bd53a71867e74df
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-02-15 20:53:53 -0500

    cleanup; moved and renamed some scripts

--- Assets/Scripts/DestroyByBoundary.cs
+++ Assets/Scripts/DestroyByBoundary.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestroyByBouundary : MonoBehaviour {
+public class DestroyByBoundary : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
