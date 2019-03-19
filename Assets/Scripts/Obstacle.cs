ac52293b25c1cbe8afbb4238b123260f4c612e11
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-03-18 20:33:59 -0400

    added basic vertical speed and forward boosting

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private Ship ship;
+
+    void Awake()
+    {
+        ship = GameObject.FindWithTag("PlayerShip").GetComponent<Ship>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, -ship.vertSpeed, 0);
+    }
+}
---
+++ Assets/Scripts/Ship.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    const float defHorizSpeed = 5f;
+    private float defVertSpeed = 0.15f;
+    private float boostLen = 0.1f;
+    private float boostHorizSpeedMult = 6f;
+    private float boostVertStr = 0.05f;
+    private float horizSpeed = defHorizSpeed;
+    public float vertSpeed;
+    private float horizAxis;
+    private Vector3 move;
+    private bool boosting = false;
+
+    void Awake()
+    {
+        vertSpeed = defVertSpeed;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // can only change direction while not boosting
+        if (!IsBoosting())
+        {
+            move = new Vector3(horizAxis, 0, 0);
+            // normalize movement vector so joysticks don't have an advantage
+            // in precision
+            move.Normalize();
+        }
+
+        transform.position += move * horizSpeed * Time.deltaTime;
+    }
+
+    public void Boost()
+    {
+        if (horizAxis == 0f)
+            vertSpeed += boostVertStr;
+
+        horizSpeed = defHorizSpeed * boostHorizSpeedMult;
+        
+        boosting = true;
+        Invoke("StopBoost", boostLen);
+    }
+
+    public void StopBoost()
+    {
+        horizSpeed = defHorizSpeed;
+        boosting = false;
+    }
+
+    public bool IsBoosting()
+    {
+        return boosting;
+    }
+
+    public void SetHoriz(float axis)
+    {
+        horizAxis = axis;
+    }
+}
