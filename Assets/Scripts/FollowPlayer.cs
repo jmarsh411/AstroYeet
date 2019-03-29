9493dd8dc5440b4175c4424ed439a03c818afb62
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-03-29 18:17:22 -0400

    added camera controller to follow player ship

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class FollowPlayer: MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called once per frame, after all other updates
+    void LateUpdate()
+    {
+        transform.position = new Vector3(0, player.transform.position.y) + offset;
+    }
+}
