c6a4f5b5a514c1e881998cd49a36c833af8b293a
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-03-31 15:31:53 -0400

    some camera updates to center the play area

---
+++ Assets/Scripts/InitBoundSize.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitBoundSize : MonoBehaviour
+{
+    private GameManager game;
+    private BoxCollider2D box;
+
+    private void Awake()
+    {
+        game = Camera.main.GetComponent<GameManager>();
+        box = GetComponent<BoxCollider2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        box.offset = game.playArea.center;
+        box.size = game.playArea.size;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
