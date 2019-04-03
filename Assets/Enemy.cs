71f2aa27793623c8e228d743142e2a65705b62f4
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-04-02 23:19:33 -0400

    moved laser spawning code to Enemy script

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,25 +4,45 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public GameObject LaserPrefab;
+
     private GameObject player;
     private Ship ship;
+    private Rigidbody2D playerRB;
+    private Rigidbody2D enemyRB;
     private const float startDist = 900;
+    private float fastVelMult;
 
     private void Awake()
     {
         player = GameObject.FindWithTag("PlayerShip");
         ship = player.GetComponent<Ship>();
+        enemyRB = GetComponent<Rigidbody2D>();
+        playerRB = ship.GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
         transform.position -= new Vector3(0, startDist, 0);
+        fastVelMult = 1.2f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        enemyRB.velocity = playerRB.velocity * fastVelMult;
+
+        //temporary testing key
+        if (Input.GetKeyUp(KeyCode.L))
+        {
+            ShootLaser(transform.position);
+        }
     }
+
+    public void ShootLaser(Vector3 position)
+    {
+        Instantiate(LaserPrefab, position, Quaternion.identity);
+    }
+
 }
