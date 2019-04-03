ae070b5f15a83385f2ead02439351911e2c099f6
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-04-02 22:47:41 -0400

    added enemy prefab and script

---
+++ Assets/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private GameObject player;
+    private Ship ship;
+    private const float startDist = 900;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("PlayerShip");
+        ship = player.GetComponent<Ship>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position -= new Vector3(0, startDist, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
