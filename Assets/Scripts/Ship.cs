da61e222505cceedd3597d3de561de2d27302cc9
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-04-04 19:01:25 -0400

    added controller brake support

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -63,9 +63,9 @@ public class Ship : MonoBehaviour
         {
             Accel();
         }
-        
-        // if (Input.GetAxis("BrakeJoy") > 0 || Input.GetAxis("BrakeKey") > 0)
-        if (Input.GetAxis("BrakeKey") > 0)
+
+        if (Input.GetAxis("BrakeJoy") > 0 || Input.GetAxis("BrakeKey") > 0)
+        //if (Input.GetAxis("BrakeKey") > 0)
         {
             Decel();
         }
