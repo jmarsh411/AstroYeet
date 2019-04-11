877ef7fac834939014fae7672ef9da91c6f3dc06
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-04-10 23:29:19 -0400

    added test background scrolling

---
+++ Assets/ScrollBG.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBG : MonoBehaviour
+{
+    public float scrollSpeed;
+    private GameObject player;
+    private Vector3 startPos;
+    private float tileSizeY = 20;
+
+    private void Awake()
+    {
+        player = GameObject.FindWithTag("PlayerShip");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+        transform.position = player.transform.position + Vector3.down * newPosition;
+        //transform.position = player.transform.position + Vector3.up * newPosition;
+    }
+}
