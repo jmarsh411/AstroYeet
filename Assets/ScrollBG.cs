aa9702a769abab515109c31829d7f94cf0f2225a
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-04-13 13:26:44 -0400

    added foreground and middleground stars

--- Assets/ScrollBG.cs
+++ Assets/ScrollBG.cs
@@ -6,26 +6,31 @@ public class ScrollBG : MonoBehaviour
 {
     public float scrollSpeed;
     private GameObject player;
-    private Vector3 startPos;
+    private Rigidbody2D playerRB;
     private float tileSizeY = 20;
     private Vector3 offset = new Vector3(0, 10 + 8, 0);
+    private float playerY;
 
     private void Awake()
     {
         player = GameObject.FindWithTag("PlayerShip");
+        playerRB = player.GetComponent<Rigidbody2D>();
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
-        transform.position = player.transform.position + offset + Vector3.down * newPosition;
-        //transform.position = player.transform.position + Vector3.up * newPosition;
+        float tileY = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+        playerY = player.transform.position.y;
+        Vector3 newPos = new Vector3(0, playerY);
+        //float tileY = Mathf.Repeat(Time.time * scrollSpeed * playerRB.velocity.y, tileSizeY);
+
+        transform.position = newPos + offset + Vector3.down * tileY;
+        //transform.position = player.transform.position + Vector3.up * tileY;
     }
 }
