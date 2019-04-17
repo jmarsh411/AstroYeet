bf5f0b025de8677f77760e005ca397126f60e471
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-04-16 20:13:21 -0400

    made laser warnings spawn more accurately when enemies are closer

--- Assets/Scripts/SpawnLasers.cs
+++ Assets/Scripts/SpawnLasers.cs
@@ -8,9 +8,14 @@ public class SpawnLasers : MonoBehaviour {
     [Header("Set in Inspector")]
     public GameObject LaserPrefab;
     public GameObject LaserWarnPrefab;
+    private GameManager game;
     public float spawnPerSec = 1f;
     public float spawnPadding = 1.5f;
     public Vector3 spawnZone;
+    private const float minAccSpread = 0.5f;
+    private float maxAccSpread;
+    private float accFalloff;
+    float acc;
 
     private GameObject player;
     private GameObject enemyShip;
@@ -19,34 +24,55 @@ public class SpawnLasers : MonoBehaviour {
 
     void Awake()
     {
+        game = Camera.main.GetComponent<GameManager>();
         player = GameObject.FindWithTag("PlayerShip");
         enemyShip = GameObject.FindWithTag("EnemyShip");
         enemy = enemyShip.GetComponent<Enemy>();
         Invoke("SpawnWarning", (enemy.dist + 75) / 50);
     }
 
+    private void Start()
+    {
+        maxAccSpread = game.playArea.extents.x;
+        accFalloff = GameManager.playerHeadStart / 5f;
+    }
+
+
     void SpawnLaserOLDER()
     {
         Vector3 spawnPos = new Vector3(Random.Range(-spawnZone.x, spawnZone.x) + uiOffset, spawnZone.y, spawnZone.z);
         //Instantiate(LaserWarnPrefab, spawnPos, Quaternion.identity);
         Instantiate(LaserPrefab, spawnPos, Quaternion.identity);
-        Invoke("SpawnLaserOLD", 1f / spawnPerSec);
+        Invoke("SpawnLaserOLDER", 1f / spawnPerSec);
     }
 
     void SpawnWarningOLD()
     {
         Vector3 spawnPos = new Vector3(Random.Range(-spawnZone.x, spawnZone.x) + uiOffset, spawnZone.y, spawnZone.z);
         Instantiate(LaserWarnPrefab, spawnPos, Quaternion.identity);
-        Invoke("SpawnWarning", 1f / spawnPerSec);
+        Invoke("SpawnWarningOLD", 1f / spawnPerSec);
     }
 
-    void SpawnWarning()
+    void SpawnWarning3()
     {
         Vector3 spawnPos = new Vector3(Random.Range(-spawnZone.x, spawnZone.x) + uiOffset, player.transform.position.y + playerOffset, spawnZone.z);
         Instantiate(LaserWarnPrefab, spawnPos, Quaternion.identity);
         // minimum of 1.5 seconds for 0 distance
         // want about 10 seconds for distance 400
-        Invoke("SpawnWarning", (enemy.dist  + 75) / 50);
+        Invoke("SpawnWarning3", (enemy.dist + 75) / 50);
+    }
+
+    void SpawnWarning()
+    {
+        float spread = Mathf.Clamp(enemy.dist / accFalloff, minAccSpread, maxAccSpread);
+        Vector3 spawnPos = new Vector3(
+            player.transform.position.x + Random.Range(-spread, spread),
+            player.transform.position.y + playerOffset,
+            0);
+        Instantiate(LaserWarnPrefab, spawnPos, Quaternion.identity);
+        // minimum of 1.5 seconds for 0 distance
+        // want about 10 seconds for distance 400
+        Invoke("SpawnWarning", (enemy.dist + 75) / 50);
     }
 
     public void SpawnLaser(Vector3 position)
