00338992bb681231aef81464d1bdadfd20e1c3a1
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-04-17 12:37:54 -0400

    fixed spawn hole; asteroids now spawn in full span of playable space

--- Assets/Scripts/SpawnAsteroids.cs
+++ Assets/Scripts/SpawnAsteroids.cs
@@ -11,18 +11,36 @@ public class SpawnAsteroids : MonoBehaviour {
     public float spawnPerSec = 1f;
     public float spawnPadding = 1.5f;
     public Vector3 spawnZone;
+
+    private GameManager game;
     private Rigidbody2D playerRB;
+    private float colliderPadding;
+    private float leftBound;
+    private float rightBound;
+
 
     void Awake()
     {
+        game = Camera.main.GetComponent<GameManager>();
         player = GameObject.FindWithTag("PlayerShip");
         playerRB = player.GetComponent<Rigidbody2D>();
+        //spawn an asteroid off screen to get the size of its collider
+        GameObject temp = Instantiate(AsterPrefab, new Vector3(-100, -100), Quaternion.identity);
+        CapsuleCollider2D coll = temp.GetComponent<CapsuleCollider2D>();
+        colliderPadding = coll.bounds.extents.x;
+        Destroy(temp);
+    }
+
+    private void Start()
+    {
+        leftBound = game.playArea.min.x + colliderPadding;
+        rightBound = game.playArea.max.x - colliderPadding;
         Invoke("SpawnAster", 1f / spawnPerSec);
     }
 
     void SpawnAster()
     {
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnZone.x, spawnZone.x) + uiOffset, spawnZone.y + player.transform.position.y, spawnZone.z);
+        Vector3 spawnPos = new Vector3(Random.Range(leftBound, rightBound), spawnZone.y + player.transform.position.y, 0);
         Instantiate(AsterPrefab, spawnPos, Quaternion.identity);
         float speedmult = playerRB.velocity.y / 4;
         Invoke("SpawnAster", 1f / spawnPerSec / speedmult  * Random.Range(0.4f, 1.6f));
