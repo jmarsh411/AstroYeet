6ce06c8f34758f3a3c9e9146a6049aed82bf8726
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-06-09 14:11:12 -0400

    re-enabled the ship collider and removed old comment

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -24,10 +24,6 @@ public class GameManager : MonoBehaviour
         cam = GetComponent<Camera>();
         float camHeight = cam.orthographicSize * 2;
         float camWidth = camHeight * cam.aspect;
-        // camera width is 11.25
-        // UI rect is 50 wide out of a total 450 width, or 1/9
-        // 11.25 * 1/9 = 1.25
-        // play area width = 11.25 - 1.25 = 10
         playArea = new Bounds(Vector3.zero, new Vector3(camWidth, camHeight, 0));
     }
 
