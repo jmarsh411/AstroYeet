3a535501ca2466e4e763b40afb9a9a454da2da07
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-06-18 23:18:10 -0400

    added test particle system and a means to lerp its box's scale
    - this unfortunately didn't have the effect of moving the particles
      like it does in preview mode, but it can serve as a base for the
      correct effect

---
+++ Assets/Scripts/ParticleScaleLerp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScaleLerp : MonoBehaviour
+{
+    public Vector3 targetScale;
+
+    private Vector3 origScale;
+    private ParticleSystem partSys;
+    private ParticleSystem.MainModule main;
+    private ParticleSystem.ShapeModule shape;
+    private Vector3 newScale;
+
+    private void Awake()
+    {
+        partSys = GetComponent<ParticleSystem>();
+        main = partSys.main;
+        shape = partSys.shape;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        origScale = shape.scale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newScale = Vector3.Lerp(origScale, targetScale, partSys.time / main.duration);
+        shape.scale = newScale;
+    }
+}
