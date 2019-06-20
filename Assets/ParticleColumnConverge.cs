16c039686e63bd39de6d99ee1b41ae15093a80a1
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-06-19 21:43:46 -0400

    bugfix for the particle converge script; tweaked particle effects

--- Assets/ParticleColumnConverge.cs
+++ Assets/ParticleColumnConverge.cs
@@ -6,6 +6,7 @@ public class ParticleColumnConverge : MonoBehaviour
 {
     private ParticleSystem pSystem;
     private ParticleSystem.Particle[] particles;
+    private Vector3[] startPositions;
     private Vector3 vertCenter;
 
     private void Awake()
@@ -16,6 +17,10 @@ public class ParticleColumnConverge : MonoBehaviour
     void Start()
     {
         particles = new ParticleSystem.Particle[pSystem.main.maxParticles];
+        startPositions = new Vector3[pSystem.main.maxParticles];
+
+        Vector3[] vArray = new Vector3[5];
+        Debug.Log("test");
     }
 
     // Update is called once per frame
@@ -23,10 +28,20 @@ public class ParticleColumnConverge : MonoBehaviour
     {
         int numParticles = pSystem.GetParticles(particles);
         for (int i = 0; i < numParticles; i++)
-            {
+        {
+
+
             ParticleSystem.Particle particle = particles[i];
+
+            // store the starting position of this particle if it's the first time
+            if (startPositions[i] == Vector3.zero)
+            {
+                startPositions[i] = particle.position;
+            }
+
             vertCenter = new Vector3(0, particle.position.y, particle.position.z);
-            particle.position = Vector3.Lerp(particle.position, vertCenter, (particle.startLifetime - particle.remainingLifetime) / particle.startLifetime);
+            //particle.position = Vector3.Lerp(particle.position, vertCenter, (particle.startLifetime - particle.remainingLifetime) / particle.startLifetime);
+            particle.position = Vector3.Lerp(startPositions[i], vertCenter, (particle.startLifetime - particle.remainingLifetime) / particle.startLifetime);
 
             particles[i] = particle;
         }
