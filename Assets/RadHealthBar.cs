dfb1be955256507a6d278293c6028f1da739e880
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-06-22 16:09:14 -0400

    added main stuff for radial health bar

---
+++ Assets/RadHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityAtoms;
+
+public class RadHealthBar : MonoBehaviour
+{
+    public FloatReference hp;
+    public FloatReference maxHP;
+
+    private Image image;
+    private float fill;
+
+    private void Awake()
+    {
+        image = GetComponent<Image>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        image.fillAmount = hp.Value / maxHP.Value;
+    }
+}
