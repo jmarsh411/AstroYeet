8b8cb5508e8894e27b3d2e84f9a25bce168e957c
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-06-22 12:53:36 -0400

    converted camera offset to scriptable object and fixed offset bug

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityAtoms;
 
     public class FollowPlayer: MonoBehaviour
 {
     private GameObject player;
-    private Vector3 offset;
+    public Vector3Reference offset;
+    public Vector3Constant defaultOffset;
 
     private void Awake()
     {
@@ -14,12 +16,12 @@ using UnityEngine;
     // Start is called before the first frame update
     void Start()
     {
-        offset = transform.position - player.transform.position;
+        offset.Variable.Value = defaultOffset.Value;
     }
 
     // LateUpdate is called once per frame, after all other updates
     void LateUpdate()
     {
-        transform.position = new Vector3(0, player.transform.position.y) + offset;
+        transform.position = new Vector3(0, player.transform.position.y) + offset.Value;
     }
 }
