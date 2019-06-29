db473e4a6f7c51edd8786639cd960c1137461de9
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-06-29 11:46:31 -0400

    added enemy shooting event and warning spawner listener

---
+++ Assets/Scripts/WarningSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms;
+
+public class WarningSpawner : MonoBehaviour, IGameEventListener<Vector3>
+{
+    public GameObject WarningPrefab;
+
+    [SerializeField]
+    private Vector3Event enemyShootEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyShootEvent.RegisterListener(this);
+    }
+
+    public void OnEventRaised(Vector3 pos)
+    {
+        //Debug.Log("Laser Spawned: " + pos);
+        GameObject laser = Instantiate(WarningPrefab, transform, false);
+        laser.transform.localPosition = pos;
+    }
+}
