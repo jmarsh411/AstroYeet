855ccc098d3af2d06a789318d132d38837f57ac9
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-07-02 17:24:29 -0400

    added script for warning particle objects to self-destruct when dead

---
+++ Assets/ParticleSysEOL.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSysEOL : MonoBehaviour
+{
+    private ParticleSystem pSystem;
+
+    void Awake()
+    {
+        pSystem = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!pSystem.IsAlive())
+            Destroy(this.gameObject);
+    }
+}
