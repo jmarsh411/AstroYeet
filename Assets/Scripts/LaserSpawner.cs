9b0ba230c50b5513fa8c75ff7b1e6462572ce558
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-07-02 17:36:43 -0400

    added laser spawning event and spawning system

---
+++ Assets/Scripts/LaserSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms;
+
+public class LaserSpawner : MonoBehaviour, IGameEventListener<Vector3>
+{
+    public GameObject LaserPrefab;
+
+    [SerializeField]
+    private Vector3Event laserSpawnEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserSpawnEvent.RegisterListener(this);
+    }
+
+    public void OnEventRaised(Vector3 pos)
+    {
+        //Debug.Log("Laser Spawned: " + pos);
+        GameObject laser = Instantiate(LaserPrefab, transform, false);
+        laser.transform.localPosition = pos;
+    }
+}
