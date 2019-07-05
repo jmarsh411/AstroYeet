af1367496e3ae4ca02a70ef51465db74a5edfcbd
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-07-05 13:52:03 -0400

    removing unused stuff in Laser script

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
-    public GameObject laserWarningPrefab;
     private GameObject player;
     private Ship ship;
-    private GameObject laserWarning;
-    private LaserWarn warn;
-
-    //private bool warning;
-    public float warnDist;
-    private float distance;
-    //private Vector3 warnOffset = new Vector3(0, -1.2f, 0);
 
     private void Awake()
     {
@@ -24,8 +16,6 @@ public class Laser : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //warning = false;
-        warnDist = 100;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -36,27 +26,4 @@ public class Laser : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
-
-    // Update is called once per frame
-    //void FixedUpdate()
-    //{
-    //    distance = player.transform.position.y - transform.position.y;
-    //    if (!warning && distance < warnDist)
-    //    {
-    //        warning = true;
-    //        Vector3 warnPos = new Vector3(transform.position.x, player.transform.position.y, 0) + warnOffset;
-    //        laserWarning = Instantiate(laserWarningPrefab, warnPos, Quaternion.identity);
-    //        warn = laserWarning.GetComponentInChildren<LaserWarn>();
-    //    }
-
-    //    if (warning && distance >= 0)
-    //    {
-    //        //warn.UpdState(distance);
-    //    }
-    //}
-
-    //IEnumerator SpawnWarning()
-    //{
-    //    yield return null;
-    //}
 }
