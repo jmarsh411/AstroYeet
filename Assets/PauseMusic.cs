f4bf942b5caebf284c086ba7443089fd53eefec8
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-07-14 14:17:41 -0400

    made music pause and resume when the game is paused

---
+++ Assets/PauseMusic.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms;
+
+public class PauseMusic : MonoBehaviour, IGameEventListener<Void>
+{
+
+    [SerializeField]
+    private VoidEvent pauseEvent;
+    private AudioSource audioSource;
+
+    private bool paused;
+
+    void Awake()
+    {
+        paused = false;
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseEvent.RegisterListener(this);
+    }
+
+    public void OnEventRaised(Void unused)
+    {
+        if (paused)
+        {
+            audioSource.Play();
+            paused = false;
+        }
+        else
+        {
+            audioSource.Pause();
+            paused = true;
+        }
+    }
+
+
+    public void OnDisable()
+    {
+        pauseEvent.UnregisterListener(this);
+    }
+}
