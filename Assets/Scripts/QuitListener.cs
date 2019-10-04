d465a24127f8febe0ad77ea6b48bbbd494c3ba4a
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-10-03 20:31:35 -0400

    added Quit Event and functionality for main menu

---
+++ Assets/Scripts/QuitListener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityAtoms;
+
+public class QuitListener : MonoBehaviour, IGameEventListener<Void>
+{
+
+    [SerializeField]
+    private VoidEvent quitEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        quitEvent.RegisterListener(this);
+    }
+
+    public void OnEventRaised(Void unused)
+    {
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void OnDisable()
+    {
+        quitEvent.UnregisterListener(this);
+    }
+}
