88d20b679082cd1c4e8ce37b0686e74892afc169
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-10-06 14:59:40 -0400

    added start game functionality to new Main Menu

---
+++ Assets/Scripts/StartGameListener.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityAtoms;
+
+public class StartGameListener : MonoBehaviour, IGameEventListener<Void>
+{
+
+    [SerializeField]
+    private VoidEvent startGameEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startGameEvent.RegisterListener(this);
+    }
+
+    public void OnEventRaised(Void unused)
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void OnDisable()
+    {
+        startGameEvent.UnregisterListener(this);
+    }
+}
