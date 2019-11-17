105acee2bb543adb9e7c25cb04385cbca2d885ee
jmarsh411 <10360503+jmarsh411@users.noreply.github.com>
2019-11-16 21:22:16 -0500

    fixed an issue where a menu button wasn't always selected

---
+++ Assets/Scripts/MainMenuInit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuInit : MonoBehaviour
+{
+
+    public Button startButon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startButon.Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
